77d0f5bfd667a11afdd81d2f621db61c120acd8e
mchris9913 <120289183+mchris9913@users.noreply.github.com>
2025-01-09 11:39:33 -0800

    Update dynamics_core.py
    Changed design philosophy for the dynamics

--- python/tiamat/dynamics_core.py
+++ python/tiamat/dynamics_core.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
-class vehicle(ABC):
-    def __init__(self, x0: np.ndarray)->None:
-        self.x = [x0]
-        return
+def translational_dynamics(x: np.ndarray, param: tuple) -> np.ndarray:
+    POS_IDX, VEL_IDX, mu = param
+    
+    xdot = np.zeros_like(x)
+    xdot[POS_IDX] = x[VEL_IDX]
+    xdot[VEL_IDX] = -mu/(np.linalg.norm(x[POS_IDX])**3) * x[POS_IDX]
+    return xdot
+
+def rotational_dynamics(x: np.ndarray, param: tuple) -> np.ndarray:
+    return
