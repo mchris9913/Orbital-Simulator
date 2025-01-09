93a994a8e56ae73b9159795d28be9c23f613ec41
mchris9913 <120289183+mchris9913@users.noreply.github.com>
2025-01-09 12:08:26 -0800

    Added rotational dynamics equations

--- python/tiamat/dynamics_core.py
+++ python/tiamat/dynamics_core.py
@@ -1,13 +1,23 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
-def translational_dynamics(x: np.ndarray, param: tuple) -> np.ndarray:
-    POS_IDX, VEL_IDX, mu = param
+def translational_dynamics(x: np.ndarray, params: tuple) -> np.ndarray:
+    """
+    r' = v
+    v' = -mu/r^3 * r
+    """
+    
+    POS_IDX, VEL_IDX, mu = params
     
     xdot = np.zeros_like(x)
     xdot[POS_IDX] = x[VEL_IDX]
     xdot[VEL_IDX] = -mu/(np.linalg.norm(x[POS_IDX])**3) * x[POS_IDX]
     return xdot
 
-def rotational_dynamics(x: np.ndarray, param: tuple) -> np.ndarray:
-    return
+def rotational_dynamics(x: np.ndarray, params: tuple) -> np.ndarray:
+    """
+    Iw' + w x Iw = M
+    """
+    I, M = params
+    xdot = np.linalg.inv(I) @ (M - np.outer(x, I @ x))
+    return x
